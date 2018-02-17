fc0202790fc739f30112f447d814db9781ed1df3
Ron Nero <ronaldc.nero@gmail.com>
2018-02-16 23:32:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+var users =[
+    {
+        name: 'Lucy',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar1.png'        
+    },
+    {
+        name: 'Betty',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar2.png'
+    },
+    {
+        name: 'Ronald',
+        gender: 'M',
+        hobby: 'music',
+        avatar: 'avatar3.png'
+    },
+    {
+        name: 'Christopher',
+        gender: 'M',
+        hobby: 'sports',
+        avatar: 'avatar4.png'
+    },
+    {
+        name: 'Ximena',
+        gender: 'F',
+        hobby: 'reading',
+        avatar: 'avatar5.png'
+    },
+    {
+        name: 'Paul',
+        gender: 'M',
+        hobby: 'shopping',
+        avatar: 'avatar6.png'
+    },
+    {
+        name: 'Charlie',
+        gender: 'M',
+        hobby: 'pets',
+        avatar: 'avatar7.png'
+    },
+]
+
+
+window.addEventListener('load', function() {
+    
+    var results = document.getElementById('results');
+
+    function search() {
+        
+        var hobbyField = document.getElementById('hobby');
+        var hobby = hobbyField.value;
+
+        var genderField = document.getElementById('gender');
+        var s = genderField.selectedIndex;
+        var gender = genderField.options[s].value;
+
+        var resultsHtml = '';
+        var usersLength = users.length;
+        for (i = 0; i < usersLength; i++) { 
+            if(gender == 'A' || gender == users[i].gender) {
+                if(hobby == '' || hobby == users[i].hobby) {
+                    resultsHtml += '<div class="person-row">\
+                        <img src="images/' + users[i].avatar + '" />\
+                        <div class="person-info">\
+                        <div>' + users[i].name + '</div>\
+                        <div>' + users[i].hobby + '</div></div>\
+                        <button>Add as friend</button></div>';
+                }
+            }
+        }
+        results.innerHTML = resultsHtml;
+    }
+
+    var searchBtn = document.getElementById('searchBtn');
+    
+    searchBtn.addEventListener('click', search);
+});
